fb9e0f488fd5342e1eaad321b92733055eef5160
felixdoura <felixdoura@hotmail.com>
2023-11-15 18:00:19 -0300

    I just added the register modal, plus the closing feature

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -11,13 +11,63 @@ import {
 } from 'react-hook-form';
 
 import useRegisterModal from '@/app/hooks/useRegisterModal';
+import Modal from './Modal';
+import Heading from '../Heading';
 
 
 const RegisterModal = () => {
+    const registerModal = useRegisterModal();
+    const [isLoading, setIsLoading] = useState(false);
+
+    const {
+        register,
+        handleSubmit,
+        formState: {
+            errors,
+        }
+    } = useForm<FieldValues>({
+        defaultValues: {
+            name: '',
+            email: '',
+            password: ''
+        }
+    });
+
+    const onSubmit: SubmitHandler<FieldValues> = (data) => {
+        setIsLoading(true);
+
+        axios.post('/api/register', data)
+            .then(() => {
+                registerModal.onClose();
+            })
+            .catch((error) => {
+                console.log(error);
+            })
+            .finally(() => {
+                setIsLoading(false);
+            })
+    }
+
+    const bodyContent = (
+        <div className='flex flex-col gap-4'>
+            <Heading 
+                title='Welcome'
+                subtitle='Create an Account!'
+            />
+            
+        </div>
+    )
+
     return ( 
-        <>
-            <div></div>
-        </>
+        <Modal 
+            disabled={isLoading}
+            isOpen={registerModal.isOpen}
+            title={"Register"}
+            actionLabel="Continue"
+            onClose={registerModal.onClose}
+            onSubmit={handleSubmit(onSubmit)}
+            body={bodyContent}
+        />
      );
 }
  
