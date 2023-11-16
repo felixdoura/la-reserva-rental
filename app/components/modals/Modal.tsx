09b3e07766b0e694cf38d6a1a637a041c366ee45
felixdoura <felixdoura@hotmail.com>
2023-11-15 23:14:32 -0300

    Finished with the register UI

--- app/components/modals/Modal.tsx
+++ app/components/modals/Modal.tsx
@@ -169,6 +169,7 @@ const Modal: React.FC<ModalProps> = ({
                                         onClick={handleSubmit}
                                     />
                                 </div>
+                                {footer}
                             </div>
                         </div>
                     </div>
